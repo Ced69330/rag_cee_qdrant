5a84ea4a8c55f02bcbae6c2122724b9b691e6e36
Ced69330 <cedric.jourdain@gmail.com>
2025-12-06 13:46:27 +0100

    Add configuration variables for Qdrant and GitHub PDF

---
+++ config.py
@@ -0,0 +1,4 @@
+QDRANT_URL = "https://cddd7863-0d84-4373-b822-fc0e98335ea5.europe-west3-0.gcp.cloud.qdrant.io:6333"
+QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.tTNN0BLJB12-xV9suHCb166ewGAPBX8gSIbQRXdTp7g"
+COLLECTION_NAME = "documents_pdf"
+GITHUB_PDF_FOLDER = "https://raw.githubusercontent.com/Ced69330/Cee-news-scrapper-/main/pdf_lettres_info_c2e/"
